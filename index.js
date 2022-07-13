54c490c781e1c9d8a26d71cba5e557e754122a2f
Georgiy-777 <demirov709079@gmail.com>
2022-07-14 00:38:02 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const burgerBtn = document.querySelector('.header__burger');
+
+const menuClose = document.querySelector('.menu-close');
+
+const menuBurger = document.querySelector('.header__nav');
+
+burgerBtn.addEventListener('click', () =>{
+    menuBurger.classList.add('burger-active');
+});
+
+menuClose.addEventListener('click', () =>{
+    menuBurger.classList.remove('burger-active');
+});
